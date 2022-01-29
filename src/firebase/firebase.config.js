6574d6320c90a4925bbb58d2ddd66e68884c97ab
FernandoFeN <fernandofenosv@gmail.com>
2022-01-29 07:22:20 -0600

    Updated firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: "todo-list-545a0",
+  projectId: "todo-fem",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID
